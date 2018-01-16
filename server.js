21a2a7f1715ed1a7c986d60fa747b95706d77009
Cyril Ajieh <>
2018-01-15 20:43:55 -0600

    implemented endpoints for categories GRT and POST

--- server.js
+++ server.js
@@ -1,8 +1,57 @@
 const express = require('express')
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 const app = express()
+
+const Category = require('./models/Category.js');
+
+mongoose.Promise = Promise;
+
+const categories  =[
+    {name:'React'},
+    {name:'Angular'}
+]
+
+// middleware
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
+
+// get categories 
+app.get('/categories', async (req, res) => {
+    try{
+         const categories = await Category.find({}, '-__v')
+         res.send(categories);
+     }catch(e){
+         console.log(e)
+         res.sendStatus(500)
+     }
+})
+
+// post to categories
+app.post('/categories', (req,res)=>{
+    
+    const category= new Category({name:req.body.name})
+    category.save((err, result) =>{
+        if(err){
+            console.error("posting category error")
+            return res.status(500).send({massage:"posting category error"})
+        }     
+        res.sendStatus(200)     
+    })
+})
+
+
+  mongoose.connect('mongodb://test:test@ds259117.mlab.com:59117/collab_db',(err) =>{
+    if(!err){
+        console.log('Connected to Mongo')
+    }
+});
+
+app.listen(3000, () => console.log('collab app ws listening on port 3000!'))
